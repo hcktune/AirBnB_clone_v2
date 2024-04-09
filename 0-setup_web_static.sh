223cbbae6a0887ec30427574b40c29d4a7678fde
Your Name <you@example.com>
2024-04-09 02:47:42 +0000

    adding chgrp

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,6 +13,7 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static_releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
