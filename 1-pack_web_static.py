27499c620396bfe8c497cc6e46f51f2af4e32c87
Your Name <you@example.com>
2024-04-09 03:14:21 +0000

    we fix the style, Exceptions

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" Generates a .tgx archive from the contents of a web_static folder"""
+""" Generates a .tgz archive from the contents of a web_static folder"""
 from fabric.api import local
 import time
 
+
 def do_pack():
-    """
-    Generate an tgz archive
-    """
+    """  Generate an tgz archive  """
     try:
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except:
+        local("tar -cvzf versions/web_static_{}.tgz web_static/"
+              .format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz"
+                .format(time.strftime("%Y%m%d%H%M%S")))
+    except Exception as e:
+        print("An Error has occured :", str(e))
         return None
