9cd8009d48704cb1c289ba6f94841443c4c1d57a
Your Name <you@example.com>
2024-04-09 02:57:28 +0000

    fix ip add

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["44.210.150.159", "35.173.47.15"]
+env.hosts = ["100.26.168.245", "34.234.193.78"]
 env.user = "ubuntu"
 
 
