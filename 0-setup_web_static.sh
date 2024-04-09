8f2adf83f4d1bdea67e88d630ce1087480a80c40
Your Name <you@example.com>
2024-04-09 02:30:39 +0000

    fix permission

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,6 +13,6 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static_releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
